c22b88cce74b43a64530436eb971fc964af9155f
Revanthsatyam <satyamrevanth@gmail.com>
2023-11-16 13:24:14 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -57,7 +57,7 @@ resource "aws_autoscaling_group" "bar" {
     version = "$Latest"
   }
 
-  tags {
+  tag {
     key                 = "Name"
     value               = local.name_prefix
     propagate_at_launch = true
