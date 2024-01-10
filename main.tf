4cac4e7eb58a4fdfdf05c2a614470a1cae42fa55
Revanthsatyam <satyamrevanth@gmail.com>
2024-01-10 16:33:39 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ resource "aws_iam_policy" "main" {
           "ssm:GetParameters",
           "ssm:GetParameter"
         ],
-        "Resource":
+        "Resource":local.policy_resources
       },
       {
         "Sid": "VisualEditor1",
