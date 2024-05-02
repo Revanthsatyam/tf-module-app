43b774b508c30a92b33611d7768cdf512519d860
Revanthsatyam <satyamrevanth@gmail.com>
2024-05-02 12:12:16 +0530

    Terraform

--- userdata.sh
+++ userdata.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 yum install ansible -y &>>/opt/userdata.log
-ansible-pull -i localhost, -U https://github.com/raghudevopsb74/roboshop-ansible.git main.yml -e component=${component} &>>/opt/userdata.log
+ansible-pull -i localhost, -U https://github.com/Revanthsatyam/roboshop-ansible.git -e component=${component} &>>/opt/userdata.log
