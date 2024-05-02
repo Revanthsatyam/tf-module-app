eb4ba1db3225f47009d3bc885d5ef25d3eca4b24
Revanthsatyam <satyamrevanth@gmail.com>
2024-05-02 16:34:13 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -88,4 +88,10 @@ resource "aws_lb_listener_rule" "main" {
     type             = "forward"
     target_group_arn = aws_lb_target_group.main.arn
   }
+
+  condition {
+    host_header {
+      values = "${var.component}-${var.env}-rdevops74.online"
+    }
+  }
 }
