3bf2e998f1875c0e2ae667637994709ee3a38f44
Revanthsatyam <satyamrevanth@gmail.com>
2024-05-02 16:12:17 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -79,3 +79,13 @@ resource "aws_lb_target_group" "main" {
   protocol = "HTTP"
   vpc_id   = var.vpc_id
 }
+
+#resource "aws_lb_listener_rule" "main" {
+#  listener_arn = var.listener_arn
+#  priority     = var.priority
+#
+#  action {
+#    type             = "forward"
+#    target_group_arn = aws_lb_target_group.main.arn
+#  }
+#}
