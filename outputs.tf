0f561fcae433a5f2413ab9244679f4f7cf65bee7
Revanthsatyam <satyamrevanth@gmail.com>
2024-05-04 11:38:39 +0530

    Terraform

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,6 @@
 #  value = data.dns_a_record_set.main
 #}
 
-output "aws_lb_target_group_public" {
-  value = aws_lb_target_group.public
-}
+#output "aws_lb_target_group_public" {
+#  value = aws_lb_target_group.public
+#}
