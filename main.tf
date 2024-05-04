7fb6e167de02c02c78e6c5a61c544ec9f22568a2
Revanthsatyam <satyamrevanth@gmail.com>
2024-05-04 14:11:23 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -100,10 +100,17 @@ resource "aws_lb_listener_rule" "main" {
 }
 
 resource "aws_lb_target_group" "public" {
-  count       = var.component == "frontend" ? 1 : 0
-  name        = "${local.name_prefix}-public"
-  port        = var.port
-  protocol    = "HTTP"
-  vpc_id      = var.default_vpc_id
+  count    = var.component == "frontend" ? 1 : 0
+  name     = "${local.name_prefix}-public"
+  port     = var.port
+  protocol = "HTTP"
+  vpc_id   = var.default_vpc_id
+}
+
+resource "aws_lb_target_group_attachment" "public" {
+  count            = var.component == "frontend" ? length(tolist(data.dns_a_record_set.private_alb_name.addrs)) : 0
+  target_group_arn = aws_lb_target_group.public[0].arn
+  target_id        = element(tolist(data.dns_a_record_set.private_alb_name.addrs), count.index)
+  port             = 80
 }
 
