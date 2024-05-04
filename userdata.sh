db45c5101b424365986b5c13c2425b0889e2b467
Revanthsatyam <satyamrevanth@gmail.com>
2024-05-04 14:49:54 +0530

    Terraform

--- userdata.sh
+++ userdata.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-yum install ansible -y &>>/opt/userdata.log
+yum install ansible python3.11-pip.noarch -y &>>/opt/userdata.log
+pip3.11 install botocore boto3
 ansible-pull -i localhost, -U https://github.com/Revanthsatyam/roboshop-ansible.git main.yml -e component=${component} &>>/opt/userdata.log
