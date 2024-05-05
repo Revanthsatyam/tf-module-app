dce61aa6ccec081dacb045ad3c71600b83865beb
Revanthsatyam <satyamrevanth@gmail.com>
2024-05-05 12:31:06 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -83,6 +83,11 @@ resource "aws_iam_role_policy_attachment" "main" {
   policy_arn = aws_iam_policy.main.arn
 }
 
+resource "aws_iam_instance_profile" "test_profile" {
+  name = "${local.name_prefix}-role"
+  role = aws_iam_role.main.name
+}
+
 resource "aws_launch_template" "main" {
   name                   = local.name_prefix
   image_id               = data.aws_ami.ami.id
