9ef438db2850edea203bc8d24730bbe83ca751a7
Revanthsatyam <satyamrevanth@gmail.com>
2024-06-05 12:49:28 +0530

    KMS

--- main.tf
+++ main.tf
@@ -121,15 +121,15 @@ resource "aws_launch_template" "main" {
       env       = var.env
     }))
 
-  block_device_mappings {
-    device_name = "/dev/sda1"
-
-    ebs {
+#   block_device_mappings {
+#     device_name = "/dev/sda1"
+#
+#     ebs {
 #       volume_size           = 10
-      encrypted             = true
+#       encrypted             = true
 #       kms_key_id            = var.kms_key_id
-    }
-  }
+#     }
+#   }
 
   tag_specifications {
     resource_type = "instance"
