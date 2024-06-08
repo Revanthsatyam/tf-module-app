c6c660b05853a025522efb982f0e6aa691c430ba
Revanthsatyam <satyamrevanth@gmail.com>
2024-06-08 18:53:46 +0530

    AMI

--- userdata.sh
+++ userdata.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-yum install ansible python3.12-pip.noarch -y &>>/opt/userdata.log
-pip3.12 install botocore boto3
+#yum install ansible python3.12-pip.noarch -y &>>/opt/userdata.log
+#pip3.12 install botocore boto3
 ansible-pull -i localhost, -U https://github.com/Revanthsatyam/roboshop-ansible.git main.yml -e component=${component} -e env=${env} &>>/opt/userdata.log
